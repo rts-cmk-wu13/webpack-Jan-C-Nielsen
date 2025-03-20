a9a99a56145effb2848b7462008547ecf61acd2d
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-03-20 12:53:56 +0100

    SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,44 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
+    mode: 'development',
+    entry: {
+
+      
+        print: {
+            import: './src/print.js',
+            dependOn: 'shared',
         },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
         },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
         },
-      },
+        shared: 'lodash',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
     ],
-  },
- };
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+
+};
 
